a3b2f462876c22faf0a7d53e52c7cae52db6c554
Daanyal Parbulkar <93346079+daanyal011@users.noreply.github.com>
2023-01-12 19:48:40 +0530

    Add files via upload

---
+++ number prediction game.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int number, guess, num_guess = 1;
+    srand(time(0));
+    number = rand() % 100 + 1; // generate number between 1-100
+    
+
+    while (guess != number)
+    {
+        printf("Guess the number between 1 to 100\n");
+        scanf("%d", &guess);
+        if (guess > number)
+        {
+            printf("Lower number please!\n");
+        }
+        else if (guess < number)
+        {
+            printf("Higher number please!\n");
+        }
+        else
+        {
+            printf("You guessed it in %d attempts\n", num_guess);
+        }
+        num_guess++;
+    }
+
+    return 0;
+}

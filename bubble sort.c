93219c3feef90aea30304b72279457b680e77f88
Daanyal Parbulkar <93346079+daanyal011@users.noreply.github.com>
2023-01-24 22:01:14 +0530

    Update bubble sort.c

--- bubble sort.c
+++ bubble sort.c
@@ -1,59 +1,59 @@
 /* Aim:Write a program in C to sort elements of the
 array in ascending order.
 Author: Daanyal*/
-#include<stdio.h>
+#include <stdio.h>
 void scan_array(int arr[], int n)
 {
     int i;
-    for(i=0;i<n;i++)
+    for (i = 0; i < n; i++)
     {
-        printf("enter  element %d: ", i+1);
+        printf("enter  element %d: ", i + 1);
         scanf("%d", &arr[i]);
     }
 }
-void print_array(int arr[],int n)
+void print_array(int arr[], int n)
 {
     int i;
 
     printf("[");
-    for(i=0;i<n;i++)
+    for (i = 0; i < n; i++)
+    {
+        printf("%d", arr[i]);
+        if (i < n - 1)
         {
-            printf("%d", arr[i] );
-            if(i<n-1)
+            printf(",");
+        }
+    }
+    printf("]");
+}
+void sort_array(int a[], int n)
+{
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < (n - i) - 1; j++)
+        {
+            if (a[j] > a[j + 1])
             {
-               printf(",");
+                temp = a[j + 1];
+                a[j + 1] = a[j];
+                a[j] = temp;
             }
         }
-        printf("]");
-}
-void sort_array(int a[],int n)
-{
-    int i,j,temp;
-    for(i=0;i<n-1;i++)
-    {
-        for(j=0;j<(n-i)-1;j++)
-        {
-          if(a[j]>a[j+1])
-          {
-              temp=a[j+1];
-              a[j+1]=a[j];
-              a[j]=temp;
-           }
-       }
     }
 }
 int main()
 {
-    int a[100],n;
+    int a[100], n;
     printf("Enter the number of elements: ");
     scanf("%d", &n);
-    scan_array(a,n);
+    scan_array(a, n);
     printf("original array: ");
-    print_array(a,n);
-    sort_array(a,n);
-    printf("\nSorted array: ") ;
-    print_array(a,n);
-   return 0;
+    print_array(a, n);
+    sort_array(a, n);
+    printf("\nSorted array: ");
+    print_array(a, n);
+    return 0;
 }
 /* Output: Enter the number of elements: 5
 enter  element 1: 8
